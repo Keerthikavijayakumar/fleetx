a32925ec39a58e63fdcd7aac2a56113a9d7996ce
Keerthikavijayakumar <keerthikav.23cse@kongu.edu>
2026-03-26 00:38:18 +0530

    added ui url

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5173', 'http://localhost:3000'],
+        origin: ['http://localhost:5173', 'http://localhost:3000','https://fleetx-nu.vercel.app/'],
         methods: ['GET', 'POST'],
     },
 });
@@ -38,7 +38,7 @@ setDriverIO(io);
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://fleetx-nu.vercel.app/'],
     credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
